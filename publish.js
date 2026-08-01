7d9600a515d7cd039920a425ab339bc722b67b8a
heilss1 <heilss2233@gmail.com>
2026-08-01 20:11:32 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -42,7 +42,7 @@ var urls=[
     'nqxaftal.com,
 ];                                                                                                                  
 
-var JumpPage="https://heiliaoshe5.net";
+var JumpPage="https://heiliaoshe6.net";
 
 var newestUrls = [];
 
