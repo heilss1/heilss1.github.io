d16c3156e5efd3ef078deec809f2a489451fbf4a
heilss1 <heilss2233@gmail.com>
2026-08-24 22:31:46 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,7 +39,7 @@ var emails = [
 var urls=[
 	'arrvsbrv.cc/', 
 	'bsevialyj.cc/', 
-    'cdpogegch.cc/,
+    'ffnkdoka.cc/,
 ];                                                                                                                  
 
 var JumpPage="https://heiliaoshe7.net";
